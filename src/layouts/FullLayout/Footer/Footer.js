50a857355d507aead1974eb6f6420fe6a72aa48a
sezerdemir7 <ddemir1221@gmail.com>
2025-01-19 17:36:17 +0300

    update

--- src/layouts/FullLayout/Footer/Footer.js
+++ src/layouts/FullLayout/Footer/Footer.js
@@ -7,7 +7,13 @@ import {
   } from "@mui/material";
 const Footer = () => {
     return ( 
-        <Box sx={{p:3, textAlign:'center'}}>
+        <Box sx={{ p: 3, textAlign: 'center' }}>
+            <Typography variant="body2" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
+                İnönü Üniversitesi Dijital Dönüşüm Ofisi
+            </Typography>
+            <Typography variant="body2" sx={{ fontStyle: 'italic', color: 'text.secondary' }}>
+                powered by kv97
+            </Typography>
         </Box>
      );
 }
